81713a02deba65c915aad2cad8b3caf70c12ade7
Sanguine <ret2zp@gmail.com>
2018-04-06 15:58:13 +0900

    Add files via upload

---
+++ search.php
@@ -0,0 +1,22 @@
+<?php
+class SynoDLMSearchTorrentKim {
+	private $qurl = "https://torrentkim.pro/bbs/rss.php?k=";
+
+	public function __construct() {
+	}
+
+	public function prepare($curl, $query) {
+		$url = $this->qurl . urlencode($query);
+		curl_setopt($curl, CURLOPT_URL, $url);
+	}
+
+	public function parse($plugin, $response) {
+		$response = preg_replace("/<pubDate>/i", "<pubDate>" . date("r"), $response);
+		$response = preg_replace("/<\/pubDate>/i", "</pubDate><category>All</category>", $response);
+		$response = preg_replace("/<description><\/description>/i", "<description><![CDATA[Category: All<br />Subcategory: All]]></description>", $response);
+		$response = preg_replace("/\&dn=.*?(?=&tr)/i", "", $response);
+		$response = preg_replace("/&/i", "%26", $response);
+		return $plugin->addRSSResults($response);
+	}
+}
+?>
